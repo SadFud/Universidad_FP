5cffd7d539f002e9a2b1bc12da15fe628e468eae
David C <SadFud@users.noreply.github.com>
2016-10-15 19:05:50 +0200

    Add files via upload

--- tema 2 ejercicio 11/tema 2 ejercicio 11/Main.cpp
+++ tema 2 ejercicio 11/tema 2 ejercicio 11/Main.cpp
@@ -4,15 +4,50 @@ using namespace std;
 int main(){
 
 	int a, b, c;
-	cout << "introduce 3 numeros: " << endl;
-	cin >> a;
-	cin >> b;
-	cin >> c;
 
-	if (a < b < c)[
-	
-		cout << 
-	]
+	cout << "Introduzca el primer numero entero: " << endl;
+	cin >> a;
+	cout << "Introduzca el segundo numero entero: " << endl;
+	cin >> b;
+	cout << "Introduzca el tercer numero entero: " << endl;
+	cin >> c;
+	cout << endl;
+
+	if (a < b && a < c) {
+		cout << a << endl;
+		if (b < c) {
+			cout << b << endl;
+			cout << c << endl;
+		}
+		if (c < b) {
+			cout << c << endl;
+			cout << b << endl;
+		}
+	}
+
+
+	if (b < a && b < c) {
+		cout << b << endl;
+		if (a < c) {
+			cout << a << endl;
+			cout << c << endl;
+		}
+		if (c < a) {
+			cout << c << endl;
+			cout << a << endl;
+		}
+	}
+
+	if (c < a && c < b) {
+		cout << c << endl;
+		if (a < b) {
+			cout << a << endl;
+			cout << b << endl;
+		}
+		if (b < a)
+			cout << b << endl;
+		cout << a << endl;
+	}
 
 	return 0;
 }
